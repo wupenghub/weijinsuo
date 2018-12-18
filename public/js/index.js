2a6a6c1a67455c9c19f885b9dcff86ed90a3270a
wupengforIT <wupengforIT@163.com>
2018-12-18 12:00:15 +0800

    轮播图在移动端的手势完成

--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,28 @@ $(function () {
         isMobile = width < 768;
         show();
     }).trigger('resize');
+    var startX = 0;
+    var distanceX = 0;
+    var width = $('.wjs-babber').width();
+    $('.wjs-babber').on('touchstart',function (e) {
+        startX = e.originalEvent.touches[0].clientX;
+    });
+    $('.wjs-babber').on('touchmove',function (e) {
+        distanceX = e.originalEvent.touches[0].clientX - startX;
+    });
+    $('.wjs-babber').on('touchend',function (e) {
+        if(distanceX < 0 ){
+            // 往左移动
+            if(Math.abs(distanceX) > width/4){
+                $('.carousel').carousel('next');
+            }
+        }else{
+            // 往右移动
+            if(Math.abs(distanceX) > width/4){
+                $('.carousel').carousel('prev');
+            }
+        }
+    });
 });
 function show() {
     if(window.data){
